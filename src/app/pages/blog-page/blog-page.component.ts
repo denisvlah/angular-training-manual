778e8d81c861d422ff623c89fcf74f48ee3555f9
denis.vlah <denis.vlah@kapto.ai>
2025-02-18 22:35:22 +0200

    more progress

--- src/app/pages/blog-page/blog-page.component.ts
+++ src/app/pages/blog-page/blog-page.component.ts
@@ -1,20 +1,30 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { PostComponent } from "./post/post.component";
 import { MaterialModule } from '../../material.module';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { AccountService, ApplicationPostSchemasPostReadSchema, PostService } from '../../data/services/rest';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-blog-page',
-  imports: [PostComponent, MaterialModule, ReactiveFormsModule, MatButtonModule],
+  imports: [PostComponent, MaterialModule, ReactiveFormsModule, MatButtonModule, AsyncPipe],
   templateUrl: './blog-page.component.html',
   styleUrl: './blog-page.component.scss'
 })
-export class BlogPageComponent {
-  
+export class BlogPageComponent implements OnInit {
+  posts: ApplicationPostSchemasPostReadSchema[] = [];
+  ngOnInit(): void {
+    this.postService.getPostsPostGet()
+      .subscribe(p => this.posts = p);
+
+  }
+
   public static readonly PATH = ''
 
   fb = inject(FormBuilder);
+  postService = inject(PostService);
+  accountService = inject(AccountService);
 
   form = this.fb.group({
     postText: ['', Validators.minLength(3)]
@@ -24,8 +34,13 @@ export class BlogPageComponent {
     return this.form.valid;
   }
 
-  snePost() {
-
+  sendPost() {
+    if (this.form.valid) {
+      this.postService.createPostPostPost({
+        title: this.form.value.postText!,
+      })
+        .subscribe(p => this.posts.unshift(p));
+    }
   }
 
   postTextInvalid() {
--- src/app/pages/blog-page/post/post.component.ts
+++ src/app/pages/blog-page/post/post.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MaterialModule } from '../../../material.module';
+import { ApplicationPostSchemasPostReadSchema } from '../../../data/services/rest';
 
 @Component({
   selector: 'app-post',
@@ -8,5 +9,7 @@ import { MaterialModule } from '../../../material.module';
   styleUrl: './post.component.scss'
 })
 export class PostComponent {
+  
+  @Input() post!: ApplicationPostSchemasPostReadSchema;
 
 }
