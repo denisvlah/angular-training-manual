b01a7e6e391c3b970ec6f625f5e8d485a9402a6a
denis.vlah <denis.vlah@kapto.ai>
2025-02-18 12:28:35 +0200

    progress

--- src/app/pages/blog-page/blog-page.component.ts
+++ src/app/pages/blog-page/blog-page.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PostComponent } from "./post/post.component";
+import { MaterialModule } from '../../material.module';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-blog-page',
-  imports: [PostComponent],
+  imports: [PostComponent, MaterialModule, ReactiveFormsModule, MatButtonModule],
   templateUrl: './blog-page.component.html',
   styleUrl: './blog-page.component.scss'
 })
 export class BlogPageComponent {
+  
   public static readonly PATH = ''
+
+  fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    postText: ['', Validators.minLength(3)]
+  })
+
+  canSend() {
+    return this.form.valid;
+  }
+
+  snePost() {
+
+  }
+
+  postTextInvalid() {
+    return this.form.touched && this.form.value.postText && this.form.value.postText.length < 3
+  }
+
+
+
 
 }
